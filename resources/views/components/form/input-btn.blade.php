f73cd07c7a35de3d60945171d11114c6d03a07b5
Anju Alba Sitompul <anjuluvmotis13@gmail.com>
2026-04-12 14:16:47 +0700

    fix tutton

--- resources/views/components/form/input-btn.blade.php
+++ resources/views/components/form/input-btn.blade.php
@@ -15,7 +15,7 @@
 
 <label for="{{ $id }}" class="form-label">{{ $label }}</label>
 <div class="input-group has-validation">
-    <button type="button" id="{{ $btnId }}" class="btn btn-primary" data-bs-toggle="{{ $toggle }}"
+    <button type="button" id="{{ $btnId }}" class="btn btn-primary" data-bs-toggle="{{ $toggle || '' }}"
         data-bs-target="{{ $target }}">{{ $btnTxt }}</button>
     <input type="{{ $type }}" id="{{ $id }}" name="{{ $name }}" class="form-control"
         placeholder="{{ $placeholder }}" {{ $disabled ? 'disabled' : '' }}>
