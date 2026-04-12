7b2d46d2250277a1a1da4d4c44192d3ec57e4aed
Anju Alba Sitompul <anjuluvmotis13@gmail.com>
2026-04-12 14:23:47 +0700

    fix tutton

--- resources/views/pages/barang.blade.php
+++ resources/views/pages/barang.blade.php
@@ -54,7 +54,8 @@
                                         <x-form.input-btn placeholder="Masukkan Code SJ" btnTxt="Scan"
                                             btnId="suratjalanBtn" name="codesj" type="basic"
                                             invalid="Harap Masukkan Code SJ" label="Surat Jalan" id="sj"
-                                            disabled="{{ false }}" value="tag1, tag2 autofocus" />
+                                            disabled="{{ false }}" value="tag1, tag2 autofocus" toggle=""
+                                            target="" />
                                     </div>
                                     <button type="submit" class="btn btn-primary mb-2 me-4 ">Tambahkan Picker</button>
                                 </form>
